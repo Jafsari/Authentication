407626a126d09f240a6d493092436b8ada841874
Jafsari <justinafsari@yahoo.com>
2018-02-12 14:50:24 -0800

    added bcrypt, and ability to save users to the database

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,29 @@
+const User = require('../models/user')
+
 exports.signup = function(req,res,next){
-	res.send({success: ' true'})
+  const email = req.body.email;
+  const password = req.body.password;
+    
+  if (!email || !password){
+    return res.status(422).send({error:'You must provide an email and password'});
+  }
+  //See if a given user email exists already
+  User.findOne({email:email}, function(err,existingUser){
+// check if their is an error connecting, to send an error back
+      if (err) {return next(err);}
+// check if the email is already in use send an error back
+      if(existingUser){
+        return res.status(422).send({error:'This email is already in use'});
+      }
+// If the email is not in use, lets create it and save it to the database.
+      const user = new User({
+          email:email,
+          password:password
+      });
+      user.save(function(err){
+        if (err) {return next(err);}
+      
+      res.json(user);
+    });
+  });
 }
